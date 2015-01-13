a326244b076e336d2dc6274844ee1e92be1e533b
amandawraymond <amandawraymond@gmail.com>
2015-01-12 23:33:17 -0500

    add to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-
-User.create(name:"Amanda Raymond", email:"amandawraymond@gmail.com", phone_number:8042918214, street_address:"105 sycamore place", city:"Decatur", state:"GA")
+User.all.destroy
+User.create(name:"Amanda Raymond", email:"amandawraymond@gmail.com", phone_number:8042918214, street_address:"105 sycamore place", city:"Decatur", state:"GA"g),
+User.create(name:"Carson Raymond", email:"carsonraymond@gmail.com", phone_number:8044759597, street_address:"105 sycamore place", city:"Decatur", state:"GA"),
+User.create(name:"Chris Markel", email:"cbm@gmail.com", phone_number:4042180454, street_address:"1110 Church St", city:"smyrna", state:"GA"),
+User.create(name:"Coleman", email:"coleman@gmail.com", phone_number:8044759597, street_address:"3562 piedmont rd NE", city:"Atlanta", state:"GA"),
+User.create(name:"Gerry Pass", email:"gerrypass@gmail.com", phone_number:4049539976, street_address:"273 Buckhead Avenue Northeast", city:"Atlanta", state:"GA")
